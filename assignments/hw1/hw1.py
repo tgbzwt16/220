d8fdccb9be8c03f0f7e0152e479b4b55cf099ff1
tgbzwt16 <97422158+tgbzwt16@users.noreply.github.com>
2022-01-19 14:45:52 -0500

    homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,8 +1,8 @@
 """
-Name: <your name goes here – first and last>
+Name: Thomas Thornley
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Creating functions with inputs to allow users to perform different computations.
 
 Certification of Authenticity:
 <include one of the following>
@@ -12,24 +12,38 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    shots_taken = eval(input("Enter the amount taken: "))
+    shots_made = eval(input("Enter the amount made: "))
+    shooting_per = (shots_made/shots_taken)*100
+    print("shooting percentage =", shooting_per)
 
 
 def coffee():
-    pass
+    amount = eval(input("How much pounds of coffee would you like? "))
+    cof_per_lb = 10.5
+    ship_cost = 0.86
+    overhead_cost = 1.5
+    total_cost = amount*(cof_per_lb+ship_cost)+overhead_cost
+    print("total cost =", round(total_cost, 2))
 
 
 def kilometers_to_miles():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+    amount = eval(input("How many kilometers did you travel? "))
+    kl_to_mi = 1.61
+    total = amount / kl_to_mi
+    print("total miles =", total)
