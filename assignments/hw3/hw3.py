003600afd71f32ff907f74b4a59e2f9864fb965d
tgbzwt16 <97422158+tgbzwt16@users.noreply.github.com>
2022-02-04 23:43:17 -0500

    homework 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,8 +1,8 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Thornley
+hw3.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Using loops to write functions.
 
 Certification of Authenticity:
 <include one of the following>
@@ -12,24 +12,54 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
+    question = eval(input("How many grades will you enter? "))
+    sum_ = 0
+    for i in range(question):
+        grade = eval(input("Enter grade: "))
+        sum_ = sum_ + grade
+        total = sum_ / question
+        i -= 1
+    print(total)
 
 
 def tip_jar():
-    pass
+    total = 0
+    ppl_tipping = 5
+    for i in range(ppl_tipping):
+        tips = eval(input("How much would you like to donate? "))
+        total += tips
+        i -= 1
+    print(total)
 
 
 def newton():
-    pass
+    var1 = eval(input("What number do you want to square root? "))
+    times = eval(input("How many times should we improve the approximation? "))
+    initial = var1
+    for i in range(times):
+        initial = (initial + var1/initial)/2
+        i -= 1
+    print(initial)
 
 
 def sequence():
-    pass
+    terms = eval(input("How many terms would you like? "))
+    for i in range(1, terms+1):
+        while i % 2 == 0:
+            i = i-1
+        print(i, end=' ')
 
 
 def pi():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+    series = eval(input("How many terms in the series? "))
+    multi = 1
+    j = 1
+    i = 1
+    for i in range(2, series + 2):
+        while i % 2 == 0:
+            i = i - 1
+    for j in range(2, series + 2):
+        while j % 2 == 1:
+            j = j - 1
+    multi = multi * (j / i) * (j / i)
+    print(2*multi)
