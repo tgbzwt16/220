0239bdd8f49433aa0717ea3e71480cddbcb11822
tgbzwt16 <97422158+tgbzwt16@users.noreply.github.com>
2022-02-11 21:53:34 -0500

    homework 4

--- assignments/hw4/hw4.py
+++ assignments/hw4/hw4.py
@@ -1,8 +1,8 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Thomas Thornley
+hw4.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Creating visual programs and approximating pi in a new way
 
 Certification of Authenticity:
 <include one of the following>
@@ -10,55 +10,121 @@ I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
+import math
 from graphics import *
 
 
 def squares():
-    # Creates a graphical window
+
     width = 400
     height = 400
     win = GraphWin("Clicks", width, height)
 
-    # number of times user can move circle
     num_clicks = 5
 
-    # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to draw square")
     instructions.draw(win)
 
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    shape = Rectangle(Point(50, 50), Point(20, 20))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
-    # allows the user to click multiple times to move the circle
     for i in range(num_clicks):
         click = win.getMouse()
-        center = shape.getCenter()  # center of circle
+        center = shape.getCenter()
 
-        # move amount is distance from center of circle to the
-        # point where the user clicked
         change_x = click.getX() - center.getX()
         change_y = click.getY() - center.getY()
-        shape.move(change_x, change_y)
 
+        shape2 = Rectangle(Point(50, 50), Point(20, 20))
+        shape2.setOutline("red")
+        shape2.setFill("red")
+        shape2.draw(win)
+        shape2.move(change_x, change_y)
+
+    msg = Text(Point(200, 200), 'Click again to close')
+    msg.draw(win)
     win.getMouse()
     win.close()
 
 
 def rectangle():
-    pass
+    width = 400
+    height = 400
+    win = GraphWin("Clicks", width, height)
+
+    click1 = win.getMouse()
+    x1 = click1.getX()
+    y1 = click1.getY()
+    click2 = win.getMouse()
+    x2 = click2.getX()
+    y2 = click2.getY()
+
+    shape = Rectangle(Point(x1, y1), Point(x2, y2))
+    shape.setOutline('black')
+    shape.setFill('green')
+    shape.draw(win)
+
+    area = (x2 - x1) * (y2 - y1)
+    perimeter = (2 * (x2 - x1)) + (2 * (y2 - y1))
+
+    inst_pt1 = Point(width / 2, height - 30)
+    instructions1 = Text(inst_pt1, "Perimeter: " + str(abs(perimeter)))
+    instructions1.draw(win)
+
+    inst_pt = Point(width / 2, height - 10)
+    instructions = Text(inst_pt, "Area: " + str(abs(area)))
+    instructions.draw(win)
+
+    msg = Text(Point(200, 200), 'Click again to close')
+    msg.draw(win)
+    win.getMouse()
+    win.close()
 
 
 def circle():
-    pass
+    width = 400
+    height = 400
+    win = GraphWin("Clicks", width, height)
+
+    click1 = win.getMouse()
+    x1 = click1.getX()
+    y1 = click1.getY()
+
+    click2 = win.getMouse()
+    x2 = click2.getX()
+    y2 = click2.getY()
+
+    x_total = (x2 - x1) ** 2
+    y_total = (y2 - y1) ** 2
+    distance = (x_total + y_total) ** (1/2)
+
+    shape = Circle(click1, distance)
+    shape.setOutline("black")
+    shape.setFill("light blue")
+    shape.draw(win)
+
+    inst_pt = Point(width / 2, height - 10)
+    instructions = Text(inst_pt, "radius: " + str(distance))
+    instructions.draw(win)
+
+    msg = Text(Point(200, 200), 'Click again to close')
+    msg.draw(win)
+    win.getMouse()
+    win.close()
 
 
 def pi2():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+    terms = eval(input('Enter the number of terms to sum: '))
+    den = 1
+    pi = 0
+    sign = 1
+    for i in range(terms):
+        pi += 4 / den * sign
+        den += 2
+        sign = sign * -1
+        i -= 1
+    print(pi)
+    print(abs(math.pi - pi))
